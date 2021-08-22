513496512e39b2cdc065d5ee41f169466281c556
awee01 <85651950+awee01@users.noreply.github.com>
2021-08-21 20:20:12 -0400

    removed unused code

--- index.js
+++ index.js
@@ -11,37 +11,79 @@ const promptQuestions = [
     type: 'input',
     name: 'title',
     message: 'What is the title of your project?',
-   
+    validate: titleInput => {
+      if (titleInput) {
+        return true;
+      } else {
+        console.log('Please enter the title of your project.');
+        return false;
+      }
+    }
   },
   {
     type: 'input',
     name: 'description',
     message: 'Describe your project.',
-    
+    validate: descriptionInput => {
+      if (descriptionInput) {
+        return true;
+      } else {
+        console.log('Please provide a description of your project.');
+        return false;
+      }
+    }
   },
   {
     type: 'input',
     name: 'installation',
     message: 'Explain how to install your project.',
-    
+    validate: installationInput => {
+      if (installationInput) {
+        return true;
+      } else {
+        console.log('Please provide instructions on how to install your project.');
+        return false;
+      }
+    }
   },
   {
     type: 'input',
     name: 'usage',
     message: 'Explain how your project is meant to be used',
-    
+    validate: usageInput => {
+      if (usageInput) {
+        return true;
+      } else {
+        console.log('Please provide usage information');
+        return false;
+      }
+    }
   },
   {
     type: 'input',
     name: 'contributions',
     message: 'Suggest how users can contribute to the project',
-   
+    validate: contributeInput => {
+      if (contributeInput) {
+        return true;
+      } else {
+        console.log('Please provide information on how users can contribute to the project');
+        return false;
+      }
+    }
   },
   {
     type: 'input',
     name: 'tests',
     message: 'Provide test instructions for your project.',
-   
+    validate: testsInput => {
+      if (testsInput) {
+        return true;
+      } else {
+        console.log('Please provide test instructions for your project.');
+        return false;
+      }
+    }
   },
   {
     type: 'list',
@@ -54,24 +96,33 @@ const promptQuestions = [
     type: 'input',
     name: 'username',
     message: 'Enter your GitHub Username ',
-    
+    validate: usernameInput => {
+      if (usernameInput) {
+        return true;
+      } else {
+        console.log('Please enter your GitHub username!');
+        return false;
+      }
+    }
   },
   {
     type: 'input',
     name: 'email',
     message: 'Enter your email address',
-    
+    validate: emailInput => {
+      if (emailInput) {
+        return true;
+      } else {
+        console.log('Please enter your email address.');
+        return false;
+      }
+    }
   },
 ];
 
 // Function to write README file
-function writeREADME(fileName, data) {
-  fs.writeFile(fileName, data, (err) => {
-    if (err) {
-      reject(err);
-      return;
-    }
-  });
+function writeREADME(savefileName, data) {
+  fs.writeFile(savefileName, data, () => {});
   
   console.log('Your README has been generated, check the dist folder')
 };
